437c6275eefc9c7251e4c7d4ab17c7ae6b0f31e7
Lindsay Garner <gar21034@byui.edu>
2023-05-25 14:09:44 -0600

    css on inventory

--- models/inventory-model.js
+++ models/inventory-model.js
@@ -17,9 +17,9 @@ async function getClassifications(){
 async function getInventoryByClassificationId(classification_id) {
   try {
     const data = await pool.query(
-      "SELECT * FROM public.inventory AS i" + 
+      "SELECT * FROM public.inventory AS i " + 
       "JOIN public.classification AS c " +
-      "ON i.classification_id = c.classification_id" +
+      "ON i.classification_id = c.classification_id " +
       "WHERE i.classification_id = $1",
       [classification_id]
     )
