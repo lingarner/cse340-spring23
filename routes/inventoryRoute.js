45e10925c148f69e3de70b030f55a8149adcd737
Lindsay Garner <gar21034@byui.edu>
2023-05-26 09:59:17 -0600

    enhancement 3 task 2

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,5 +1,6 @@
 // Needed Resources 
 const express = require("express")
+const utilities = require("../utilities/")
 
 //creates a new Router object 
 //we also have routes in the server.js file
@@ -7,16 +8,18 @@ const express = require("express")
 // smaller and more manageable 
 const router = new express.Router() 
 
-const invController = require("../controllers/invController")
+const invController = require("../controllers/invController");
 
 
 // Route to build inventory by classification view
 // this route must match the routes in the links for the nav bar 
 // /inv accounted for in server.js route to here
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors((invController.buildByClassificationId)));
 
 
 // Route to build car details view
-router.get("/detail/:invId", invController.buildByInvId);
+router.get("/detail/:invId", utilities.handleErrors((invController.buildByInvId)));
+
+
 
 module.exports = router;
